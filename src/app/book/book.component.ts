e0bea4a2c52fdc258844784241d4f38333fd7717
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-02-24 18:12:59 +0200

    add Book Component

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-book',
+  templateUrl: './book.component.html',
+  styles: []
+})
+export class BookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
