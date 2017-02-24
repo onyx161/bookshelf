241c502ee56d120f351283e13da6ab0d1b8fe43f
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-02-24 11:23:43 +0200

    add firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { BookshelfComponent } from './bookshelf/bookshelf.component';
 import { BookshelfService } from "./shared/bookshelf.service";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCQybCZoCID_mOJ1LMC-GSAPmVjP4dpnMo",
+  authDomain: "bookshelf-5a065.firebaseapp.com",
+  databaseURL: "https://bookshelf-5a065.firebaseio.com",
+  storageBucket: "bookshelf-5a065.appspot.com",
+  messagingSenderId: "571729145068"
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +26,8 @@ import { BookshelfService } from "./shared/bookshelf.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [BookshelfService],
   bootstrap: [AppComponent]
